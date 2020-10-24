279e3f7fff344cf750f8634dbab977862cbc8f8d
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2020-10-24 11:07:10 +0300

    added Nav.js styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Banner from "./Banner";
+import Nav from "./Nav";
 import requests from "./requests";
 import Row from "./Row";
 
 function App() {
   return (
     <div className="app">
-      {/* Nav */}
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={` nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/240px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      {/* <img
+        className="nav__avatar"
+        src=""
+        alt="Netflix Avatar"
+      /> */}
+    </div>
+  );
+}
+
+export default Nav;
